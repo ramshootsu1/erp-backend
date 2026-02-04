8a198e2b5cb1661494b551df0cec1ffffb96c867
itsme-ram <skitram@gmail.com>
2026-02-05 02:32:00 +0530

    some changes

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+
+  app.enableCors({
+  origin: true, // allow all origins
+  credentials: true,
+  });
+  await app.listen(port, '0.0.0.0');
+
 }
 bootstrap();
